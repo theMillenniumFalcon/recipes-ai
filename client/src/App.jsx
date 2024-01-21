25315d783c3f9c0cb0eabb50c87dea5baad812d7
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-01-21 14:56:48 +0530

    added some imports in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ClerkProvider } from "@clerk/clerk-react"
 
 import DefaultLayout from './layouts/DefaultLayout.jsx'
 import AuthSignIn from "./pages/auth/AuthSignIn.jsx"
 import AuthSignUp from "./pages/auth/AuthSignUp.jsx"
+import { DialogProvider } from "./providers/dialogContext.jsx"
 
 if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -11,27 +13,33 @@ const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path='/auth/signin' 
-          element={
-            <DefaultLayout>
-              <AuthSignIn />
-            </DefaultLayout>
-          } 
-        />
+    <div className="App bg-[#FBFCFF]">
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <DialogProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route 
+                path='/auth/signin' 
+                element={
+                  <DefaultLayout>
+                    <AuthSignIn />
+                  </DefaultLayout>
+                } 
+              />
 
-        <Route 
-          path='/auth/signup' 
-          element={
-            <DefaultLayout>
-              <AuthSignUp />
-            </DefaultLayout>
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
+              <Route 
+                path='/auth/signup' 
+                element={
+                  <DefaultLayout>
+                    <AuthSignUp />
+                  </DefaultLayout>
+                } 
+              />
+            </Routes>
+          </BrowserRouter>
+        </DialogProvider>
+      </ClerkProvider>
+    </div>
   )
 }
 
