34737cb6eaa7907b81f5a14047416b1b0d1ad71e
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-01-23 03:21:44 +0530

    added imports in app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ClerkProvider } from "@clerk/clerk-react"
 
 import DefaultLayout from './layouts/DefaultLayout.jsx'
+import DefaultSecuredLayout from './layouts/DefaultSecuredLayout.jsx'
+import Index from './pages/index.jsx'
 import AuthSignIn from "./pages/auth/AuthSignIn.jsx"
 import AuthSignUp from "./pages/auth/AuthSignUp.jsx"
+import RecipeView from './pages/recipe/view/RecipeView.jsx'
+import RecipeAdd from './pages/recipe/add/RecipeAdd.jsx'
+import RecipeEdit from './pages/recipe/edit/RecipeEdit.jsx'
+import RecipeSubmission from './pages/recipe/submission/RecipeSubmission.jsx'
+import AccountMyRecipes from './pages/account/myrecipes/AccountMyRecipes.jsx'
 import { DialogProvider } from "./providers/dialogContext.jsx"
+import { RecipeProvider } from "./providers/recipeContext.jsx"
 
 if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -15,29 +23,84 @@ function App() {
   return (
     <div className="App bg-[#FBFCFF]">
       <ClerkProvider publishableKey={clerkPubKey}>
-        <DialogProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route 
-                path='/auth/signin'
-                element={
-                  <DefaultLayout>
-                    <AuthSignIn />
-                  </DefaultLayout>
-                } 
-              />
+        <RecipeProvider>
+          <DialogProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route 
+                  index 
+                  element={
+                    <DefaultLayout>
+                      <Index />
+                    </DefaultLayout>
+                  } 
+                />
 
-              <Route 
-                path='/auth/signup' 
-                element={
-                  <DefaultLayout>
-                    <AuthSignUp />
-                  </DefaultLayout>
-                } 
-              />
-            </Routes>
-          </BrowserRouter>
-        </DialogProvider>
+                <Route 
+                  path='/recipe/view/:idx' 
+                  element={
+                    <DefaultLayout>
+                      <RecipeView />
+                    </DefaultLayout>
+                  } 
+                />
+
+                <Route 
+                  path='/recipe/add' 
+                  element={
+                    <DefaultSecuredLayout>
+                      <RecipeAdd />
+                    </DefaultSecuredLayout>
+                  } 
+                />
+
+                <Route 
+                  path='/recipe/edit/:idx' 
+                  element={
+                    <DefaultSecuredLayout>
+                      <RecipeEdit />
+                    </DefaultSecuredLayout>} 
+                />
+
+                  <Route 
+                    path='/recipe/submission/:idx' 
+                    element={
+                      <DefaultSecuredLayout>
+                        <RecipeSubmission />
+                      </DefaultSecuredLayout>
+                    } 
+                  />
+
+                <Route 
+                  path='/auth/signin'
+                  element={
+                    <DefaultLayout>
+                      <AuthSignIn />
+                    </DefaultLayout>
+                  } 
+                />
+
+                <Route 
+                  path='/auth/signup' 
+                  element={
+                    <DefaultLayout>
+                      <AuthSignUp />
+                    </DefaultLayout>
+                  } 
+                />
+
+                <Route 
+                  path='/account/myrecipes' 
+                  element={
+                    <DefaultSecuredLayout>
+                      <AccountMyRecipes />
+                    </DefaultSecuredLayout>
+                  } 
+                />
+              </Routes>
+            </BrowserRouter>
+          </DialogProvider>
+        </RecipeProvider>
       </ClerkProvider>
     </div>
   )
