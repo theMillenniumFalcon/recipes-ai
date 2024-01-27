709a7fbca1bd33562dda94ba40ea7d7e5bd356c9
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-01-27 16:49:25 +0530

    added worker

--- scheduled-workers/nori/src/actions.js
+++ scheduled-workers/nori/src/actions.js
@@ -1,3 +1,5 @@
+import utilsObj from "./utils"
+
 const actionsObj = {}
 
 actionsObj.cleanSuccessLogs = async(collection, olderThan) => {
@@ -13,6 +15,51 @@ actionsObj.cleanSuccessLogs = async(collection, olderThan) => {
         console.log(`... Cleaned ${result.deletedCount} success logs.`)
     } catch (error) {
         console.log('[ERR] Unable to clean success logs:')
+        console.log(error)
+    }
+}
+
+actionsObj.cleanHangingImages = async (collection, olderThan, env) => {
+    try {
+        console.log('Trying to clean hanging images...')
+        let documents = await collection.find({ 
+            success: "true", 
+            is_pending: true, 
+            updatedAt: {
+                $lt: new Date(new Date().getTime() - olderThan)
+            }
+        })
+        
+        let count = 0
+        for (let document of documents) {
+            if (document.img_url)	{
+                let imageID = doc.img_url.split('/').pop()
+
+                // Delete the image from cloudflare images
+                utilsObj.deleteImage(imageID, env)
+                let result = await collection.deleteOne({ _id: doc._id })
+                count++       
+            }
+        }
+        console.log(`... Cleaned ${count} hanging images.`)
+    } catch (error) {
+        console.log('[ERR] Unable to clean hanging images:')
+        console.log(error)
+    }
+}
+
+actionsObj.cleanUnsuccessfulLogs = async (collection, olderThan) => {
+    try {
+        console.log('Trying to clean unsuccessful logs...')
+        let result = await collection.deleteMany({ 
+            success: "false", 
+            updatedAt: {
+                $lt: new Date(new Date().getTime() - olderThan)
+            }
+        })
+        console.log(`... Cleaned ${result.deletedCount} unsuccessful logs.`)
+    } catch (error) {
+        console.log('[ERR] Unable to clean unsuccessful logs:')
         console.log(error)
     }
 }
